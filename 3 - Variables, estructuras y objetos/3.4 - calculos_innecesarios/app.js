19ecd59c602283c298cc8883250f704e6a930972
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-29 15:05:44 -0300

    Se crear var
    Se  crea var total animales

--- 3 - Variables, estructuras y objetos/3.4 - calculos_innecesarios/app.js
+++ 3 - Variables, estructuras y objetos/3.4 - calculos_innecesarios/app.js
@@ -1,5 +1,6 @@
 var animales = ["perro", "gato", "pez"];
 
+//se agrega var total animales para calcular na ves las cantidad de datos en el arreglo
 var totalAnimales = animales.length;
 
 for (var i = 0; i < totalAnimales; i++) {
