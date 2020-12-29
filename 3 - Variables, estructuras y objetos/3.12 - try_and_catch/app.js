19728e1502183862d0aec2505b4bb4ba7b49f9e0
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-29 15:23:41 -0300

    Se agrega try
    Se agrega try and cash a ciclo for

--- 3 - Variables, estructuras y objetos/3.12 - try_and_catch/app.js
+++ 3 - Variables, estructuras y objetos/3.12 - try_and_catch/app.js
@@ -1,7 +1,7 @@
 var animales = ["perro", "gato", "pez"];
 
 var totalAnimales = animales.length;
-
+// no es necesario realizar un try and cash por cada iteracion del for
 try {
      //Acciones a realizar por la excepción
     for (var i = 0; i < totalAnimales; i++) {
